25e51ee3c92ebf98f61b56607dd25a4dcae21e27
Kaif1119 <mohdkaifsiddiqui1196@gmail.com>
2026-06-17 17:35:31 +0530

    feat: Add food model, repository, service, and route for food management

---
+++ Backend/src/models/food.model.js
@@ -0,0 +1,75 @@
+import mongoose from "mongoose";
+
+/* FoodSchema {
+  restaurantId
+  name
+  image
+  description
+  price
+  category
+  isVeg
+  isAvailable
+  preparationTimeInMinutes
+  rating
+} */
+
+const foodSchema = new mongoose.Schema({
+  restaurantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "restaurants",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+    default: "",
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  isVeg: {
+    type: Boolean,
+    default: true,
+  },
+  isAvailable: {
+    type: Boolean,
+    default: true,
+  },
+  preparationTimeInMinutes: {
+    type: Number,
+    default: 20,
+    min: 0,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 5,
+  },
+},
+{
+    timestamps: true,
+}
+);
+
+
+const foodModel = mongoose.model("foods", foodSchema);
+
+export default foodModel;
